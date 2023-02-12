5efe9643536493f064007becdf9afc2255467e59
Gustavo Muñoz <timbergus@gmail.com>
2023-02-12 17:06:47 +0100

    Make all variable.

--- src/App.tsx
+++ src/App.tsx
@@ -7,56 +7,37 @@ import { Card } from './components/Card'
 export const App = () => {
   const [step, setStep] = useState(0)
 
+  // Index need to start in 1 and the initial order backward.
+
+  const cards = [
+    <Card className="bg-slate-200">
+      <Button onClick={() => setStep(5)}>NEXT</Button>
+    </Card>,
+    <Card className="bg-indigo-200">
+      <Button onClick={() => setStep(4)}>NEXT</Button>
+    </Card>,
+    <Card className="bg-cyan-200">
+      <Button onClick={() => setStep(3)}>NEXT</Button>
+    </Card>,
+    <Card className="bg-orange-200">
+      <Button onClick={() => setStep(2)}>NEXT</Button>
+    </Card>,
+    <Card className="bg-green-200">
+      <Button onClick={() => setStep(1)}>NEXT</Button>
+    </Card>,
+  ]
+
   return (
     <div className="w-screen h-screen flex items-center justify-center p-48">
       <div className="relative w-full h-full">
         <div className="absolute w-full h-full flex items-center justify-center">
           <Button onClick={() => setStep(0)}>RESET</Button>
         </div>
-        <Panel
-          step={step}
-          index={3}
-          className={classNames({
-            '-translate-y-16 -rotate-6': step >= 4,
-          })}
-        >
-          <Card className="bg-indigo-200">
-            <Button onClick={() => setStep(4)}>NEXT</Button>
-          </Card>
-        </Panel>
-        <Panel
-          step={step}
-          index={2}
-          className={classNames({
-            '-translate-y-5 -rotate-2': step >= 3,
-          })}
-        >
-          <Card className="bg-cyan-200">
-            <Button onClick={() => setStep(3)}>NEXT</Button>
-          </Card>
-        </Panel>
-        <Panel
-          step={step}
-          index={1}
-          className={classNames({
-            'translate-y-5 rotate-2': step >= 2,
-          })}
-        >
-          <Card className="bg-orange-200">
-            <Button onClick={() => setStep(2)}>NEXT</Button>
-          </Card>
-        </Panel>
-        <Panel
-          step={step}
-          index={0}
-          className={classNames('', {
-            'translate-y-16 rotate-6': step >= 1,
-          })}
-        >
-          <Card className="bg-green-200">
-            <Button onClick={() => setStep(1)}>NEXT</Button>
-          </Card>
-        </Panel>
+        {cards.map((card, index) => (
+          <Panel step={step} index={index} total={cards.length}>
+            {card}
+          </Panel>
+        ))}
       </div>
     </div>
   )
--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,24 +1,47 @@
 import classNames from 'classnames'
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react'
+
+const MAX_ANGLE = 30
 
 type CardProps = {
   step: number
   index: number
-  className: string
+  total: number
   children: ReactNode
 }
 
-export const Panel = ({ step, index, className, children }: CardProps) => (
-  <div
-    className={classNames(
-      'absolute w-full h-full transition-all duration-500',
-      {
-        'scale-50': step < index,
-        '-translate-x-full': step > index,
-      },
-      className
-    )}
-  >
-    {children}
-  </div>
-)
+export const Panel = ({ step, index, total, children }: CardProps) => {
+  const realIndex = total - 1 - index
+  const middle = Math.ceil(total / 2) + 1
+
+  const middleCard = middle % 2 !== 0 && realIndex === middle
+  const sign = middleCard ? 0 : realIndex < middle ? 1 : -1
+
+  console.log('REAL INDEX:', realIndex)
+  console.log('SIGN:', sign)
+
+  return (
+    <div
+      className={classNames(
+        'absolute w-full h-full transition-all duration-500',
+        {
+          'scale-50': step < realIndex,
+          '-translate-x-full': step > realIndex,
+        }
+      )}
+      {...(step > realIndex && {
+        style: {
+          transform:
+            step > realIndex
+              ? `translate(-105%, ${sign * (realIndex + 1) * 10}px) rotateZ(${
+                  sign * (realIndex + 1) * 5
+                }deg)`
+              : '',
+          zIndex: realIndex + 1,
+        },
+      })}
+    >
+      {children}
+    </div>
+  )
+}
