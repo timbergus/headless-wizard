5b7d54c130ac14405b5219b3c3372a1f4f9e2544
Gustavo Muñoz <timbergus@gmail.com>
2023-02-16 21:44:48 +0100

    Style the x rays switch.

--- src/components/XRays.tsx
+++ src/components/XRays.tsx
@@ -14,7 +14,7 @@ export const XRays = ({ label, disclaimer, checked, onChange }: XRaysProps) => (
       checked={checked}
       onChange={onChange}
       className={classNames(
-        checked ? 'bg-indigo-600' : 'bg-gray-200',
+        checked ? 'bg-indigo-600' : 'bg-gray-400 dark:bg-gray-200',
         'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
       )}
     >
@@ -22,7 +22,7 @@ export const XRays = ({ label, disclaimer, checked, onChange }: XRaysProps) => (
         aria-hidden="true"
         className={classNames(
           checked ? 'translate-x-5' : 'translate-x-0',
-          'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+          'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-gray-200 dark:bg-gray-400 shadow ring-0 transition duration-200 ease-in-out'
         )}
       />
     </Switch>
